a81d1c91c50c892e539d5c1c2a084588d658ddb1
anuj9631 <anuj98426@gmail.com>
2025-10-27 11:17:46 +0530

    added sidebar

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
 
+import { AppSidebar } from "./_components/AppSidebar"
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,7 +26,6 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Provider>{children}</Provider>
-        
       </body>
     </html>
   );
