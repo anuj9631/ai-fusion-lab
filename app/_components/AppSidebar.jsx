cdba4a2e146104103db4af1b77dab895e2de7974
anuj9631 <anuj98426@gmail.com>
2025-10-27 08:39:09 +0530

    added logo in sidebar header

--- app/_components/AppSidebar.jsx
+++ app/_components/AppSidebar.jsx
@@ -5,11 +5,17 @@ import {
   SidebarGroup,
   SidebarHeader,
 } from "@/components/ui/sidebar"
+import Image from "next/image"
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader>
+        
+        <Image src={'/logo.svg'} alt="logo" width={60} height={60} 
+        className="w-[40px] h-[40px]"
+        />
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup />
         <SidebarGroup />
