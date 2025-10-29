5aa6a560eabe47756fd229b60012547150e55353
anuj9631 <anuj98426@gmail.com>
2025-10-29 16:41:33 +0530

    added input box

---
+++ app/_components/ChatinputBox.jsx
@@ -0,0 +1,33 @@
+import { Button } from '@/components/ui/button'
+import { Mic, Paperclip, Send } from 'lucide-react'
+import React from 'react'
+
+function ChatinputBox() {
+  return (
+    <div className='relative min-h-screen'>
+      {/* page content*/}
+      <div>
+
+      </div>
+      {/*Fixed chat input*/}
+      <div className='fixed bottom-0 left-0 w-full flex justify-center px-4 pb-4'>
+        <div className='w-full border rounded-xl shadow-md max-w-2xl p-4'>
+          <input type='text' placeholder='Ask me anything...' 
+          className='border-0 outline-none'
+          />
+          <div className='mt-5 flex justify-between items-center '>
+            <button className={''} variant={'ghost'} size={'icon'}>
+               <Paperclip className='h-5 w-5' />
+            </button>
+            <div className='flex gap-5'>
+              <Button variant={'ghost'} size={'icon'} ><Mic /></Button>
+              <Button  size={'icon'} className={'bg-blue-600'}> <Send /></Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ChatinputBox
--- app/page.js
+++ app/page.js
@@ -3,12 +3,13 @@
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 import Image from "next/image";
+import ChatinputBox from "./_components/ChatinputBox";
 
 export default function Home() {
   const {setTheme} =useTheme();
   return (
     <div>
-     
+     <ChatinputBox />
     </div>
   );
 }
